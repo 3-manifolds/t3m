2437de309a8d26df72e32f9fde1450683f7dc43a
Nathan Dunfield <nathan@dunfield.info>
2006-08-06 16:18:31 +0000

    fixed orientation conventions in simplex.py

--- simplex.py
+++ simplex.py
@@ -26,7 +26,7 @@ E21 = 6  # 0110 <-| | |
 F3  = 7  # 0111   | | |
 V3  = 8  # 1000   | | |  Opposite edges
 E03 = 9  # 1001 <-| | |
-E31 = 10 # 1010 <---| |
+E13 = 10 # 1010 <---| |
 F2  = 11 # 1011       |
 E32 = 12 # 1100 <-----|
 F1  = 13 # 1101
@@ -39,7 +39,7 @@ E10 = 3
 E20 = 5
 E12 = 6
 E30 = 9
-E13 = 10
+E31 = 10
 E23 = 12
 
 # Generate a bitmap from a tuple of vertices.
@@ -52,7 +52,7 @@ def bitmap(tuple):
 # This list of subsimplex names can be used for printing.
 
 SubsimplexName = ('N', 'V0', 'V1', 'E01', 'V2', 'E02', 'E12', 'F3',
-                 'V3', 'E03', 'E13', 'F2', 'E23', 'F1', 'F0', 'T')
+                 'V3', 'E03', 'E31', 'F2', 'E23', 'F1', 'F0', 'T')
 
 # A simplex is oriented like this:  
 #     1     
@@ -80,9 +80,9 @@ SubsimplexName = ('N', 'V0', 'V1', 'E01', 'V2', 'E02', 'E12', 'F3',
 # These dictionaries associate to each edge its initial vertex, terminal
 # vertex, or a tuple containing both.
 
-Tail     = { E01:V0   , E02:V0   , E21:V2   , E03:V0   , E31:V3   , E32:V3   }
-Head     = { E01:V1   , E02:V2   , E21:V1   , E03:V3   , E31:V1   , E32:V2   }
-EdgeTuple= { E01:(0,1), E02:(0,2), E21:(2,1), E03:(0,3), E31:(3,1), E32:(3,2)} 
+Tail     = { E01:V0   , E02:V0   , E21:V2   , E03:V0   , E13:V1   , E32:V3   }
+Head     = { E01:V1   , E02:V2   , E21:V1   , E03:V3   , E13:V3   , E32:V2   }
+EdgeTuple= { E01:(0,1), E02:(0,2), E21:(2,1), E03:(0,3), E13:(1,3), E32:(3,2)} 
 
 # These dictionaries associate to each edge a face containing it.  If
 # the terminal vertex of the edge is on top then the associated face
@@ -98,10 +98,10 @@ EdgeTuple= { E01:(0,1), E02:(0,2), E21:(2,1), E03:(0,3), E31:(3,1), E32:(3,2)}
 #    \|/
 
 
-RightFace  = { E01:F2 , E02:F3 , E21:F0 , E03:F1 , E31:F2 , E32:F0 }
-LeftFace   = { E01:F3 , E02:F1 , E21:F3 , E03:F2 , E31:F0 , E32:F1 }
-TopFace    = { E01:F0 , E02:F0 , E21:F2 , E03:F0 , E31:F3 , E32:F3 }
-BottomFace = { E01:F1 , E02:F2 , E21:F1 , E03:F3 , E31:F1 , E32:F2 }
+RightFace  = { E01:F2 , E02:F3 , E21:F3 , E03:F1 , E13:F2 , E32:F1 }
+LeftFace   = { E01:F3 , E02:F1 , E21:F0 , E03:F2 , E13:F0 , E32:F0 }
+TopFace    = { E01:F0 , E02:F0 , E21:F2 , E03:F0 , E13:F1 , E32:F3 }
+BottomFace = { E01:F1 , E02:F2 , E21:F1 , E03:F3 , E13:F3 , E32:F2 }
 
 # This dictionary associates to each face an edge contained in it.
 
@@ -127,7 +127,7 @@ AdjacentEdges = { E01:(E02,E03,E12,E13),
 TwoSubsimplices = (F0,F1,F2,F3)
 
 # A list of all edges:
-OneSubsimplices = (E01,E02,E21,E03,E31,E32)
+OneSubsimplices = (E01,E02,E21,E03,E13,E32)
 
 # A list of all vertices:
 ZeroSubsimplices = (V0,V1,V2,V3)
@@ -136,8 +136,9 @@ ZeroSubsimplices = (V0,V1,V2,V3)
 FaceIndex =   { F0:0,  F1:1, F2:2, F3:3 }
 
 # This dictionary maps a directed edge to the tail of its opposite.
-OppTail = {(V0,V1):V2,(V0,V2):V3,(V0,V3):V1,(V1,V2):V0,(V1,V3):V2,(V2,V3):V0,
-           (V1,V0):V3,(V2,V0):V1,(V3,V0):V2,(V2,V1):V3,(V3,V1):V0,(V3,V2):V1}
+
+OppTail = {(V0,V1):V3,(V0,V2):V1,(V0,V3):V2,(V1,V2):V3,(V1,V3):V0,(V2,V3):V1,
+           (V1,V0):V2,(V2,V0):V3,(V3,V0):V1,(V2,V1):V0,(V3,V1):V2,(V3,V2):V0}
 
 # Decide if the bitmap x represents a subset of the bitmap y
 def is_subset(x, y):
